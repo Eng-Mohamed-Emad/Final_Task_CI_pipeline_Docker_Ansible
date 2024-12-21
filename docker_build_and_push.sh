f47b8e0d69f5e1722a57a7e3a107529b1ff95b1b
Eng-Mohamed-Emad <eng.mohamed.emad111@gmail.com>
2024-12-21 18:38:59 +0200

    last script Docker build and push

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -4,15 +4,34 @@
 DOCKER_IMAGE_NAME="mohamedemadeldin101/my-weather-app-image"
 DOCKER_TAG="latest"  # You can also use Git commit hash as the tag
 
-# Log in to Docker Hub
-echo "Logging in to Docker Hub..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# If running in Jenkins, credentials will be automatically provided via Jenkins credentials store
+# If running locally, we will check if credentials are provided via environment variables
 
+# Check if DOCKER_USERNAME and DOCKER_PASSWORD are set (locally or Jenkins credentials)
+if [ -z "$DOCKER_USERNAME" ] || [ -z "$DOCKER_PASSWORD" ]; then
+  echo "Docker username or password not provided! Using Docker CLI login credentials."
+  # If credentials are not set, use Docker CLI default login (if already logged in on the local machine)
+  docker login
+else
+  echo "Logging in to Docker Hub with provided credentials..."
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fi
 
+# Check if login was successful
+if [ $? -ne 0 ]; then
+  echo "Docker login failed!"
+  exit 1
+fi
 
 # Build the Docker image
 echo "Building Docker image..."
 docker build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG .
+
+# Check if the build was successful
+if [ $? -ne 0 ]; then
+  echo "Docker image build failed!"
+  exit 1
+fi
 
 # Push the Docker image to Docker Hub
 echo "Pushing Docker image to Docker Hub..."
